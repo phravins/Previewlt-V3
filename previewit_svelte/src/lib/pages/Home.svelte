d05d41a09a114ec29cb685025db0f8913a0b91c5
phravins <phravintreuno@gmail.com>
2026-04-13 21:19:59 +0530

    Add Home.svelte

---
+++ previewit_svelte/src/lib/pages/Home.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Hero from '../components/Hero.svelte';
+  import LogosStrip from '../components/LogosStrip.svelte';
+  import HowItWorks from '../components/HowItWorks.svelte';
+  import Features from '../components/Features.svelte';
+  import Stats from '../components/Stats.svelte';
+  import Testimonials from '../components/Testimonials.svelte';
+  import Cta from '../components/Cta.svelte';
+
+  interface Props {
+    showToast: (msg: string) => void;
+  }
+  let { showToast }: Props = $props();
+</script>
+
+<div class="page page-home active">
+  <Hero />
+  <LogosStrip />
+  <HowItWorks />
+  <Features {showToast} />
+  <Stats />
+  <Testimonials />
+  <Cta />
+</div>
